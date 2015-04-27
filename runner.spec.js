51bb358810ff0dad27f170b3a61638065dbcfd90
Jakub Zmudzinski <jakub.zmudzinski@allegro.pl>
2015-04-27 08:27:28 +0200

    Simple regression test for logo

--- runner.spec.js
+++ runner.spec.js
@@ -7,14 +7,21 @@ var webdriverio = require('webdriverio').remote({
     host: 'grid.allegrogroup.com',
     port: 5555
 });
+var cssRegressionConfig = {
+    api: 'http://di-header-e2e.allegro-offer-test.pl-poz.dc5.alledc.net:9000/api/repositories/',
+    misMatchTolerance: 0.05,
+    screenWidth: [320,480,600,768,1024],
+    screenshotRoot: 'MainPage'
+};
+
+require('webdrivercss').init(webdriverio, cssRegressionConfig);
 
 jasmine.getEnv().addReporter(new SpecReporter({displaySpecDuration: true}));
 jasmine.getEnv().defaultTimeoutInterval = 30000;
 
 describe("Test allegro main page", function () {
     beforeEach(function () {
-        this.browser = webdriverio
-            .init();
+        this.browser = webdriverio.init();
     });
 
     afterEach(function (done) {
@@ -28,5 +35,24 @@ describe("Test allegro main page", function () {
             .isExisting('.logo', function(err, res) {
                 expect(res).toEqual(true, 'Logo is expected to exist');
             });
+    });
+
+    it("should check if logo looks the same", function() {
+        this.browser
+            .sync()
+            .url('http://allegro.pl')
+            .webdrivercss('MainPage', {
+                name: 'logo',
+                elem: '.logo'
+            }, function (error, response) {
+                expect(error).toEqual(undefined);
+                for (var i = 0; i < cssRegressionConfig.screenWidth.length; i++) {
+                    expect(response.logo[i].isWithinMisMatchTolerance).toEqual(
+                        true,
+                        'Header should look the same at ' + cssRegressionConfig.screenWidth[i] + 'px width'
+                    );
+                }
+            })
+            .sync();
     });
 });
